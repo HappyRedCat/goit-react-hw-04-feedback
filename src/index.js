19d0f465b00672d874382e06739d63c0fff4132c
HappyRedCat <HappyRedCat@users.noreply.github.com>
2024-02-29 19:23:30 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
 
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'components/GlobalStyle';
+import App from 'components/App';
 
 const theme = {
   colors: {
